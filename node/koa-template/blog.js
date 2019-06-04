d18f4d710ed5eda7557987b1a40e4b3be8b2f6ca
xiaokui <913233078@qq.com>
2019-06-04 21:55:55 +0800

    4

--- node/koa-template/blog.js
+++ node/koa-template/blog.js
@@ -3,31 +3,15 @@ const Koa = require('koa');
 const views = require('koa-views');
 const ejs = require('ejs');
 const path = require('path');
-const app = new Koa();
+// 处理请求数据
+const koaBody = require('koa-body');
+// 方便区分不同的路径。
 
-const user = {
-    name: '蛋蛋',
-    posts: [
-        {
-            id: 0,
-            title: '小程序踩坑指北'
-        },
-        {
-            id: 1,
-            title: 'vue.js 浅析 组件数据传递'
-        }
-    ]
-}
-const postsDetail = [
-    {
-        id: 0,
-        content: 'wechat app'
-    },
-    {
-        id: 1,
-        content: '<strong>react 表示不服</strong>'
-    }
-]
+// const router = new Router();
+const app = new Koa();
+const router = require('./router');
+// 往 ctx 上面加东西
+app.use(koaBody());
 app.use(
     views(path.join(__dirname, './views'), {
         extension: 'ejs'
@@ -36,21 +20,30 @@ app.use(
 
 //  /user 个人主页面
 //  /posts 文章详情
-app.use(async (ctx) => {
-    // 区分 页面
-    // http://localhost:8080/user
-    // console.log(ctx.path);
-    if (ctx.path === '/user') {
-        await ctx.render('user', { user });
-    } else if (ctx.path === '/posts') {
-        // http://localhost:8080/posts?id=0
-        const { id } = ctx.query;
-        const post = postsDetail.find(postItem => postItem.id == id);
-        await ctx.render('post', { post });
-    } else {
-        ctx.body = `无法处理请求`;
-    }
-})
+// router.get('/user', );
+// router.get('/posts', );
+// 
+// ....
+// app.use(async (ctx) => {
+//     // 区分 页面
+//     // http://localhost:8080/user
+//     // console.log(ctx.path);
+//     // method
+//     if (ctx.path === '/user') {
+//         await ctx.render('user', { user });
+//     } else if (ctx.path === '/posts') {
+//         // http://localhost:8080/posts?id=0
+//         const { id } = ctx.query;
+//         const post = postsDetail.find(postItem => postItem.id == id);
+//         await ctx.render('post', { post });
+//     } else {
+//         ctx.body = `无法处理请求`;
+//     }
+// })
+app
+.use(router.routes())
+.use(router.allowedMethods())
+
 app.listen(8080, () => {
     console.log('server is running 8080');
 })
