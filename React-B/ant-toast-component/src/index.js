1cdca04fc8dc6b37f4300fdef8a9ab6fc70b784b
xiaokui <913233078@qq.com>
2019-07-12 12:04:40 +0800

    react-toast-component

--- React-B/ant-toast-component/src/index.js
+++ React-B/ant-toast-component/src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// new Vue({
-//     el:'#root'
-// })
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
